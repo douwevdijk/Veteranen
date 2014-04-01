a86032ea77778d02135ab38e33cc314dc489ad92
douwevdijk <douwe@into-output.com>
2014-04-01 09:42:49 +0200

    update js

--- js/controllers.js
+++ js/controllers.js
@@ -66,6 +66,40 @@ angular.module('veteranenF.controllers', [])
 
 })
 
+.controller('lgCtrl', function($scope, $location, $timeout, $ionicLoading, auth) {
+
+	$scope.msgIn = false;
+
+	$scope.loading = function () {
+	
+		$ionicLoading.show({
+		content: 'FF Checken',
+		animation: 'fade-in',
+		animation: 'fade-in',
+		showBackdrop: true,
+		maxWidth: 200,  
+		});
+		
+	}
+		
+	$scope.chAccount = function () {
+	
+		auth.chAccount('douwevdijk@hotmail.com').success(function(data) {
+
+		  if(!data) {
+		  
+			$scope.msgIn = true;
+			$timeout(function () { $scope.msgIn = false; }, 3000);
+		  } else {
+		  
+		    $location.path('app/playlists');
+			
+		  }
+		  
+		});
+	}
+})
+
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
